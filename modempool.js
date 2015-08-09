028fee9212420446886bf5adc8257137cde1ea7f
ilhamrizqi <irs@sandiloka.com>
2015-08-09 23:21:06 +0700

    Update modempool.js

--- modempool.js
+++ modempool.js
@@ -1,34 +1,64 @@
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
 var portList = [];
+var events = require('events');
+var emitter = new events.EventEmitter();
+
+function cmd(serial, command, response)
+{
+	serial.write(command, function(){
+		serial.drain();
+	});
+}
 
 serialport.list(function (err, ports)
 {
-	console.log("Detecting serial ports...")
+	console.log("\r\nDetecting serial ports...")
 	portList = ports;
 	ports.forEach(function(port)
 	{
 		console.log("Found: "+port.comName);
+		var responseBuffer = '';
+
+
+
 		serial = new SerialPort(port.comName, 
 			{
 				baudrate: 115200,
 				buffersize: 1024,
-				parser: serialport.parsers.readline("\n")
+				parser: serialport.parsers.readline("\r\n")
 			});
 
 		serial.on('open', function()
 		{
-			console.log(port.comName + ': Port open');
-			serial.write('ATE0\r\n');			
-			serial.write('AT+CGMI\r\n');
-			serial.write('AT+CGMM\r\n');			
-			serial.write('AT+CGMR\r\n');
-			serial.write('AT+CGSN\r\n');
-		});
 
-		serial.on('data', function(data)
-		{
-			console.log(">> "+data);
+			emitter.on('response', function(response){
+				console.log(port.comName+': '+response.trim());
+			});
+
+			serial.on('data', function(data)
+			{				
+				if(data.trim() == "OK")
+				{
+					emitter.emit('response', responseBuffer);
+					responseBuffer = "";
+				}
+				else if(data.trim() == "ERROR")
+				{
+					responseBuffer = "";	
+				}
+				else
+				{
+					responseBuffer = responseBuffer + data;
+				}
+			});			
+
+			console.log(port.comName + ': Port open');
+			var response = '';
+			cmd(serial, 'ATE0\r\n', response);
+			cmd(serial, 'AT+CGMI\r\n', response);
+			cmd(serial, 'AT+CGMM\r\n', response);
+			cmd(serial, 'AT+CGSN\r\n', response);
 		});					
 	});
 });
